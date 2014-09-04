8fd37d91b7c4c071a40c0d3755fc01e2850e25ca
Sebastian Kraemer <basti.kr@gmail.com>
2014-09-04 19:19:51 +0200

    Style fix of quantumoptics.jl

--- src/quantumoptics.jl
+++ src/quantumoptics.jl
@@ -4,6 +4,7 @@ export bases, Basis, GenericBasis, CompositeBasis, FockBasis,
         states, StateVector, Bra, Ket, tensor, dagger, ⊗, basis_bra, basis_ket,
         operators, AbstractOperator, Operator, expect, identity, number, destroy, create,
                     sigmax, sigmay, sigmaz, sigmap, sigmam, spinbasis,
+        operators_lazy,
         timeevolution_simple,
         timeevolution,
         inplacearithmetic
@@ -11,6 +12,7 @@ export bases, Basis, GenericBasis, CompositeBasis, FockBasis,
 include("bases.jl")
 include("states.jl")
 include("operators.jl")
+include("operators_lazy.jl")
 include("timeevolution_simple.jl")
 include("timeevolution.jl")
 
