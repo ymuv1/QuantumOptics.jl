00f07f9ae90d1393cd05abe3d36ca4c00b24f2c1
Sebastian Kraemer <basti.kr@gmail.com>
2014-09-04 15:18:53 +0200

    Export pauli operators from main quantumoptics module.

--- src/quantumoptics.jl
+++ src/quantumoptics.jl
@@ -3,6 +3,7 @@ module quantumoptics
 export bases, Basis, GenericBasis, CompositeBasis, FockBasis,
 	   states, StateVector, Bra, Ket, tensor, dagger, ⊗, basis_bra, basis_ket,
 	   operators, AbstractOperator, Operator, expect, identity, number, destroy, create,
+	   			  sigmax, sigmay, sigmaz, sigmap, sigmam, spinbasis,
 	   timeevolution_simple,
 	   timeevolution,
 	   inplacearithmetic
