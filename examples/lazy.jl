9a70cc161c6f9850132122f0e3ac621d803ef269
Sebastian Kraemer <basti.kr@gmail.com>
2014-09-07 15:36:59 +0200

    Successfully implement lazy tensor using @ngenerate macros.

--- examples/lazy.jl
+++ examples/lazy.jl
@@ -4,7 +4,7 @@ using quantumoptics
 
 N1 = 2
 N2 = 2
-N3 = 2^8
+N3 = 2^9
 N = N1*N2*N3
 
 b1 = GenericBasis([N1])
@@ -22,28 +22,51 @@ psi2 = Ket(basis_total, rand(N))
 l1 = operators_lazy.LazyTensor(basis_total, basis_total, [1], [op1])
 l2 = operators_lazy.LazyTensor(basis_total, basis_total, [2], [op2])
 l3 = operators_lazy.LazyTensor(basis_total, basis_total, [1,2], [op1,op2])
+
 #println(full(l).data)
 #println(tensor(a, identity(basis)).data)
 
-#rho = identity(basis_total)
-#rho = tensor(basis_ket(basis, 2), basis_ket(basis,1))
-# println("a:")
-# println(a.data)
-# println("rho:")
-# println(rho.data)
-
 # println((l*rho).data)
 # println((full(l)*rho).data)
-println(vecnorm((l1*psi-full(l1)*psi).data))
-println(vecnorm((l2*psi-full(l2)*psi).data))
-println(vecnorm((l3*psi-full(l3)*psi).data))
+# println(vecnorm((l1*psi-full(l1)*psi).data))
+# println(vecnorm((l2*psi-full(l2)*psi).data))
+# println(vecnorm((l3*psi-full(l3)*psi).data))
 
 rho = tensor(psi, dagger(psi2))
-println(norm((l3*rho-full(l3)*rho).data))
+# println(norm((l3*rho-full(l3)*rho).data))
 
-full_l3 = full(l3)
+#full_l1 = full(l1)
+#full_l3 = full(l3)
 
-l3*rho
-@profile l3*rho
 #full_l3*rho
 #@time full_l3*rho
+
+#l3*rho
+#@time l3*rho
+
+#println(l3+l2)
+#println(vecnorm(((l3+l2)*psi-(l3*psi + l2*psi)).data))
+
+s = (l3+l2)
+
+#@time l3*rho
+#@time l2*rho
+
+result = s*rho
+@time s*rho
+operators_lazy.mul!(s,rho,result)
+@time operators_lazy.mul!(s,rho,result)
+#@time l3*psi
+
+result1 = l3*rho
+@time operators_lazy.mul!(l3, rho, result1)
+
+result2 = l2*rho
+@time operators_lazy.mul!(l2, rho, result1)
+
+@time (operators_lazy.mul!(l3, rho, result1); operators_lazy.mul!(l2, rho, result2); operators.iadd!(result1,result2))
+
+#@profile operators_lazy.mul!(l3, rho, result)
+
+#full_l1*psi
+#@time full_l1*psi
--- src/operators_lazy.jl
+++ src/operators_lazy.jl
@@ -1,9 +1,12 @@
 module operators_lazy
 
 using Iterators
+using Base.Cartesian
 using ..bases, ..states
 
 importall ..operators
+
+export LazyTensor, LazySum
 
 function complementary_indices(N::Int, indices::Vector{Int})
     dual_indices = zeros(Int, N-length(indices))
@@ -17,7 +20,9 @@ function complementary_indices(N::Int, indices::Vector{Int})
     return dual_indices
 end
 
-type LazyTensor <: AbstractOperator
+abstract LazyOperator <: AbstractOperator
+
+type LazyTensor <: LazyOperator
     basis_l::CompositeBasis
     basis_r::CompositeBasis
     indices::Array{Int}
@@ -38,6 +43,22 @@ type LazyTensor <: AbstractOperator
     end
 end
 
+type LazySum <: LazyOperator
+    basis_l::Basis
+    basis_r::Basis
+    operators::Array{AbstractOperator}
+
+    function LazySum{T<:AbstractOperator}(operators::Vector{T})
+        @assert length(operators)>1
+        op1 = operators[1]
+        for i = 2:length(operators)
+            @assert op1.basis_l == operators[i].basis_l
+            @assert op1.basis_r == operators[i].basis_r
+        end
+        new(op1.basis_l, op1.basis_r, operators)
+    end
+end
+
 function Base.full(x::LazyTensor)
     op_list = Operator[]
     for i=1:length(x.basis_l.bases)
@@ -51,44 +72,64 @@ function Base.full(x::LazyTensor)
 end
 
 
-function mul_suboperator(a::AbstractOperator, b::Ket, index::Int)
-    @assert(issubtype(typeof(b.basis), CompositeBasis))
-    N = length(b.basis.bases)
-    @assert(0<index<=N)
-    @assert(bases.multiplicable(a.basis_r, b.basis.bases[index]))
-    uninvolved_indices = [1:index-1, index+1:N]
-    uninvolved_shapes = b.basis.shape[uninvolved_indices]
-    # println("operator: ", a.data)
-    # println("|b>: ", b)
-    # println("uninvolved_indices: ", uninvolved_indices)
-    # println("uninvolved shapes: ", uninvolved_shapes)
-    b_data = reshape(b.data, reverse(b.basis.shape)...)
-    result_basis = CompositeBasis([b.basis.bases[1:index-1], a.basis_l, b.basis.bases[index+1:end]]...)
-    result = Ket(result_basis)
-    result_data = reshape(result.data, reverse(result.basis.shape)...)
-    broad_index_result = {(1:n) for n=result.basis.shape}
-    broad_index_b = {(1:n) for n=b.basis.shape}
-    for uninvolved_index in Iterators.product([1:n for n = uninvolved_shapes]...)
-        # println("uninvolved index: ", uninvolved_index)
-        broad_index_result[uninvolved_indices] = [uninvolved_index...]
-        broad_index_b[uninvolved_indices] = [uninvolved_index...]
-        # println("broad_index_result: ", broad_index_result)
-        # println("broad_index_b: ", broad_index_b)
-        b_splice = Ket(b.basis.bases[index], vec(b_data[reverse(broad_index_b)...]))
-        # println("bsplice: ", b_splice)
-        # println("a*bsplice", a*b_splice)
-        result_data[reverse(broad_index_result)...] = (a*b_splice).data
+@ngenerate N nothing function mul_suboperator{N}(a::Array{Complex128, 2}, b::Array{Complex128, N}, index::Int, result::Array{Complex128, N})
+    for m=1:size(a,2)
+        @nloops N i d->(1:size(b,N+1-d)) d->(if d==index && i_d!=m continue end) begin
+            for n=1:size(a,1)
+                @inbounds (@nref N result d->(N+1-d==index ? n : i_{N+1-d})) += a[n,m] * (@nref N b d->(i_{N+1-d}))
+            end
+        end
     end
-    return Ket(result_basis, vec(result_data))
+    return nothing
 end
+
+@ngenerate N nothing function mul_suboperator{N}(a::Array{Complex128, N}, b::Array{Complex128, 2}, index::Int, result::Array{Complex128, N})
+    for m=1:size(b,1)
+        @nloops N i d->(1:size(a,N+1-d)) d->(if d==index && i_d!=m continue end) begin
+            for n=1:size(b,2)
+                @inbounds (@nref N result d->(N+1-d==index ? n : i_{N+1-d})) += a[m,n] * (@nref N b d->(i_{N+1-d}))
+            end
+        end
+    end
+    return nothing
+end
+
+
 
 function *(a::LazyTensor, b::Ket)
     check_multiplicable(a.basis_r, b.basis)
+    b_shape = reverse(b.basis.shape)
+    b_data = reshape(b.data, b_shape...)
     for (op, operator_index) = zip(a.operators, a.indices)
-        b = mul_suboperator(op, b, operator_index)
+        # TODO: Doesnt work like this if basis_l != basis_r
+        tmp1 = zeros(eltype(b_data), b_shape...)
+        mul_suboperator(op.data, b_data, operator_index, tmp1)
+        b_data = tmp1
     end
-    return b
+    return Ket(a.basis_l, vec(b_data))
 end
+
+function *(a::Ket, b::LazyTensor)
+    check_multiplicable(a.basis, b.basis_l)
+    a_shape = reverse(a.basis.shape)
+    a_data = reshape(a.data, a_shape...)
+    for (op, operator_index) = zip(b.operators, b.indices)
+        # TODO: Doesnt work like this if basis_l != basis_r
+        tmp1 = zeros(eltype(a_data), a_shape...)
+        mul_suboperator(a_data, op.data, operator_index, tmp1)
+        a_data = tmp1
+    end
+    return Ket(b.basis_r, vec(a_data))
+end
+
+# function *(a::LazyTensor, b::Ket)
+#     check_multiplicable(a.basis_r, b.basis)
+#     for (op, operator_index) = zip(a.operators, a.indices)
+#         b = mul_suboperator(op, b, operator_index)
+#     end
+#     return b
+# end
+
 
 function *(a::LazyTensor, b::Operator)
     check_multiplicable(a.basis_r, b.basis_l)
@@ -100,6 +141,79 @@ function *(a::LazyTensor, b::Operator)
     return x
 end
 
+function *(a::Operator, b::LazyTensor)
+    check_multiplicable(a.basis_r, b.basis_l)
+    x = Operator(a.basis_l, b.basis_r)
+    for j=1:length(a.basis_l)
+        a_j = Ket(a.basis_l, a.data[j,:])
+        x.data[j,:] = (a_j*b).data
+    end
+    return x
+end
+
+function mul!(a::LazyTensor, b::Operator, result::Operator)
+    check_multiplicable(a.basis_r, b.basis_l)
+    for j=1:length(b.basis_r)
+        b_j = Ket(b.basis_l, b.data[:,j])
+        result.data[:,j] = (a*b_j).data
+    end
+    return result
+end
+
+function mul!(a::LazySum, b::Operator, result::Operator)
+    check_multiplicable(a.basis_r, b.basis_l)
+    tmp = Operator(result.basis_l, result.basis_r)
+    for op=a.operators
+        mul!(op, b, tmp)
+        operators.iadd!(result, tmp)
+    end
+    return result
+end
+
+*(a::LazySum, b::LazySum) = error()
+*(a::LazySum, b) = sum([op*b for op=a.operators])
+*(a, b::LazySum) = sum([a*op for op=b.operators])
+
++(a::LazyOperator, b::LazyOperator) = LazySum([a,b])
++(a::LazySum, b::LazySum) = LazySum([a.operators, b.operators])
++(a::LazySum, b::LazyOperator) = LazySum([a.operators, b])
++(a::LazyOperator, b::LazySum) = LazySum([a, b.operators])
+
++(a::LazySum, b::AbstractOperator) = LazySum([a.operators, b])
++(a::AbstractOperator, b::LazySum) = LazySum([a, b.operators])
+
+
+# function mul_suboperator(a::AbstractOperator, b::Ket, index::Int)
+#     @assert(issubtype(typeof(b.basis), CompositeBasis))
+#     N = length(b.basis.bases)
+#     @assert(0<index<=N)
+#     @assert(bases.multiplicable(a.basis_r, b.basis.bases[index]))
+
+#     uninvolved_indices = [1:index-1, index+1:N]
+#     uninvolved_shapes = b.basis.shape[uninvolved_indices]
+#     # println("operator: ", a.data)
+#     # println("|b>: ", b)
+#     # println("uninvolved_indices: ", uninvolved_indices)
+#     # println("uninvolved shapes: ", uninvolved_shapes)
+#     b_data = reshape(b.data, reverse(b.basis.shape)...)
+#     result_basis = CompositeBasis([b.basis.bases[1:index-1], a.basis_l, b.basis.bases[index+1:end]]...)
+#     result = Ket(result_basis)
+#     result_data = reshape(result.data, reverse(result.basis.shape)...)
+#     broad_index_result = {(1:n) for n=result.basis.shape}
+#     broad_index_b = {(1:n) for n=b.basis.shape}
+#     for uninvolved_index in Iterators.product([1:n for n = uninvolved_shapes]...)
+#         # println("uninvolved index: ", uninvolved_index)
+#         broad_index_result[uninvolved_indices] = [uninvolved_index...]
+#         broad_index_b[uninvolved_indices] = [uninvolved_index...]
+#         # println("broad_index_result: ", broad_index_result)
+#         # println("broad_index_b: ", broad_index_b)
+#         b_splice = Ket(b.basis.bases[index], vec(b_data[reverse(broad_index_b)...]))
+#         # println("bsplice: ", b_splice)
+#         # println("a*bsplice", a*b_splice)
+#         result_data[reverse(broad_index_result)...] = (a*b_splice).data
+#     end
+#     return Ket(result_basis, vec(result_data))
+# end
 
 # type EmbeddedOperator <: AbstractOperator
 #     basis_l::CompositeBasis
@@ -155,10 +269,5 @@ end
 #     return x
 # end
 
-# type LazySum <: LazyOperator
-#     basis_l::Basis
-#     basis_r::Basis
-#     operands::Array{AbstractOperator}
-# end
 
 end
