81116c05c13d5d5bbf71be0172594890241e8373
Sebastian Kraemer <basti.kr@gmail.com>
2014-09-11 03:20:49 +0200

    Implement new sparse matrix algorithm.

--- src/operators_sparse.jl
+++ src/operators_sparse.jl
@@ -1,7 +1,6 @@
 module operators_sparse
 
-using ..bases, ..states
-using Base.LinAlg.BLAS
+using ..bases, ..states, ..sparse
 
 importall ..operators
 
@@ -11,39 +10,41 @@ export SparseOperator
 type SparseOperator <: AbstractOperator
     basis_l::Basis
     basis_r::Basis
-    data::SparseMatrixCSC{Complex128}
+    data::SparseMatrix{Complex128}
 end
 
-SparseOperator(b::Basis, data) = SparseOperator(b, b, data)
-SparseOperator(b1::Basis, b2::Basis) = SparseOperator(b1, b2, spzeros(Complex128, length(b1), length(b2)))
-SparseOperator(b::Basis) = SparseOperator(b, b)
+SparseOperator(b::Basis, data::SparseMatrix) = SparseOperator(b, b, data)
+SparseOperator(b::Basis, data::Matrix) = SparseOperator(b, SparseMatrix(data))
+SparseOperator(Operator) = SparseOperator(Operator.basis_l, Operator.basis_r, SparseMatrix(Operator.data))
+#SparseOperator(b1::Basis, b2::Basis) = SparseOperator(b1, b2, spzeros(Complex128, length(b1), length(b2)))
+#SparseOperator(b::Basis) = SparseOperator(b, b)
 
 
-*(a::SparseOperator, b::Ket) = (check_multiplicable(a.basis_r, b.basis); Ket(a.basis_l, a.data*b.data))
-*(a::Bra, b::SparseOperator) = (check_multiplicable(a.basis, b.basis_l); Bra(b.basis_r, b.data.'*a.data))
-*(a::SparseOperator, b::SparseOperator) = (check_multiplicable(a.basis_r, b.basis_l); SparseOperator(a.basis_l, b.basis_r, a.data*b.data))
-*(a::SparseOperator, b::Operator) = (check_multiplicable(a.basis_r, b.basis_l); Operator(a.basis_l, b.basis_r, a.data*b.data))
-*(a::Operator, b::SparseOperator) = (check_multiplicable(a.basis_r, b.basis_l); Operator(a.basis_l, b.basis_r, a.data*b.data))
-*(a::SparseOperator, b::Number) = SparseOperator(a.basis_l, a.basis_r, complex(b)*a.data)
-*(a::Number, b::SparseOperator) = SparseOperator(b.basis_l, b.basis_r, complex(a)*b.data)
+# *(a::SparseOperator, b::Ket) = (check_multiplicable(a.basis_r, b.basis); Ket(a.basis_l, a.data*b.data))
+# *(a::Bra, b::SparseOperator) = (check_multiplicable(a.basis, b.basis_l); Bra(b.basis_r, b.data.'*a.data))
+# *(a::SparseOperator, b::SparseOperator) = (check_multiplicable(a.basis_r, b.basis_l); SparseOperator(a.basis_l, b.basis_r, a.data*b.data))
+# *(a::SparseOperator, b::Operator) = (check_multiplicable(a.basis_r, b.basis_l); Operator(a.basis_l, b.basis_r, a.data*b.data))
+# *(a::Operator, b::SparseOperator) = (check_multiplicable(a.basis_r, b.basis_l); Operator(a.basis_l, b.basis_r, a.data*b.data))
+# *(a::SparseOperator, b::Number) = SparseOperator(a.basis_l, a.basis_r, complex(b)*a.data)
+# *(a::Number, b::SparseOperator) = SparseOperator(b.basis_l, b.basis_r, complex(a)*b.data)
 
-/(a::SparseOperator, b::Number) = SparseOperator(a.basis_l, a.basis_r, a.data/complex(b))
+# /(a::SparseOperator, b::Number) = SparseOperator(a.basis_l, a.basis_r, a.data/complex(b))
 
-+(a::SparseOperator, b::SparseOperator) = ((a.basis_l==b.basis_l) && (a.basis_r==b.basis_r) ? SparseOperator(a.basis_l, a.basis_r, a.data+b.data) : throw(IncompatibleBases()))
+# +(a::SparseOperator, b::SparseOperator) = ((a.basis_l==b.basis_l) && (a.basis_r==b.basis_r) ? SparseOperator(a.basis_l, a.basis_r, a.data+b.data) : throw(IncompatibleBases()))
 
--(a::SparseOperator, b::SparseOperator) = ((a.basis_l==b.basis_l) && (a.basis_r==b.basis_r) ? SparseOperator(a.basis_l, a.basis_r, a.data-b.data) : throw(IncompatibleBases()))
+# -(a::SparseOperator, b::SparseOperator) = ((a.basis_l==b.basis_l) && (a.basis_r==b.basis_r) ? SparseOperator(a.basis_l, a.basis_r, a.data-b.data) : throw(IncompatibleBases()))
 
 
-tensor(a::SparseOperator, b::SparseOperator) = SparseOperator(compose(a.basis_l, b.basis_l), compose(a.basis_r, b.basis_r), kron(a.data, b.data))
+# tensor(a::SparseOperator, b::SparseOperator) = SparseOperator(compose(a.basis_l, b.basis_l), compose(a.basis_r, b.basis_r), kron(a.data, b.data))
 
-dagger(x::SparseOperator) = SparseOperator(x.basis_r, x.basis_l, x.data')
+# dagger(x::SparseOperator) = SparseOperator(x.basis_r, x.basis_l, x.data')
 #Base.full(x::Operator) = x
 
-Base.norm(op::SparseOperator, p) = norm(op.data, p)
-Base.trace(op::SparseOperator) = trace(op.data)
+# Base.norm(op::SparseOperator, p) = norm(op.data, p)
+# Base.trace(op::SparseOperator) = trace(op.data)
 
-spidentity(b::Basis) = SparseOperator(b, b, speye(Complex128, length(b)))
-spidentity(b1::Basis, b2::Basis) = Operator(b1, b2, eye(Complex128, length(b1), length(b2)))
+# spidentity(b::Basis) = SparseOperator(b, b, speye(Complex128, length(b)))
+# spidentity(b1::Basis, b2::Basis) = Operator(b1, b2, eye(Complex128, length(b1), length(b2)))
 # number(b::Basis) = Operator(b, b, diagm(map(Complex, 0:(length(b)-1))))
 # destroy(b::Basis) = Operator(b, b, diagm(map(Complex, sqrt(1:(length(b)-1))),1))
 # create(b::Basis) = Operator(b, b, diagm(map(Complex, sqrt(1:(length(b)-1))),-1))
@@ -60,32 +61,11 @@ spidentity(b1::Basis, b2::Basis) = Operator(b1, b2, eye(Complex128, length(b1), 
 # set!(a::Operator, b::Operator) = (check_equal_bases(a, b); set!(a.data, b.data); a)
 # zero!(a::Operator) = fill!(a.data, zero(eltype(a.data)))
 
-function operators.gemm!{T<:Complex}(alpha::T, A::SparseOperator, b::Matrix{T}, beta::T, result::Matrix{T})
-    
-    a = A.data
-    if abs(beta)<1e-14
-        fill!(result, zero(T))
-    end
-    nzv = a.nzval
-    rv = a.rowval
-    colptr = a.colptr
-    for multivec_col=1:size(b,2)::Int, col=1:a.n::Int
-        Xc = b[col::Int, multivec_col::Int]
-        @inbounds for k = colptr[col::Int]::Int : (colptr[col::Int+1]::Int-1)
-           result[rv[k::Int]::Int, multivec_col::Int]::Complex128 += alpha::Complex128 * nzv[k::Int]::Complex128 * Xc::Complex128
-        end
-    end
-    return nothing
+function operators.gemm!{T<:Complex}(alpha::T, M::SparseOperator, b::Matrix{T}, beta::T, result::Matrix{T})
+    sparse.gemm!(alpha, M.data, b, beta, result)
 end
-function operators.gemm!{T<:Complex}(alpha::T, X::Matrix{T}, a::SparseOperator, beta::T, result::Matrix{T})
-    A = a.data
-    if abs(beta)<1e-14
-        fill!(result, zero(T))
-    end
-    @inbounds for multivec_row=1:size(X,1), col=1:A.n::Int, k=A.colptr[col::Int]::Int:(A.colptr[col::Int+1]::Int-1)
-        result[multivec_row::Int, col::Int] += alpha::Complex128 * X[multivec_row::Int, A.rowval[k::Int]::Int]::Complex128 * A.nzval[k::Int]::Complex128
-    end
-    return nothing
+function operators.gemm!{T<:Complex}(alpha::T, a::Matrix{T}, M::SparseOperator, beta::T, result::Matrix{T})
+    sparse.gemm!(alpha, a, M.data, beta, result)
 end
 #gemm!{T<:Complex}(alpha::T, a::SparseOperator, b::Matrix{T}, beta::T, result::Matrix{T}) = gemm!(alpha, a.data, b, beta, result)
 #gemm!{T<:Complex}(alpha::T, a::Matrix{T}, b::SparseOperator, beta::T, result::Matrix{T}) = gemm!(alpha, a, b.data, beta, result)
---
+++ src/sparsematrix.jl
@@ -0,0 +1,56 @@
+module sparse
+
+export SparseMatrix
+
+type SparseMatrix{T}
+    index_l::Vector{Int}
+    index_r::Vector{Int}
+    values::Vector{T}
+end
+
+function SparseMatrix{T}(a::Matrix{T})
+    index_l = Int[]
+    index_r = Int[]
+    values = T[]
+    for j=1:size(a,2), i=1:size(a,1)
+        if abs2(a[i,j]) > 1e-15
+            push!(index_l, i)
+            push!(index_r, j)
+            push!(values, a[i,j])
+        end
+    end
+    return SparseMatrix(index_l, index_r, values)
+end
+
+function _fMB{T}(j::Int, index_l::Vector{Int}, index_r::Vector{Int}, values::Vector{T}, alpha::T, B::Matrix{T}, result::Matrix{T})
+    @inbounds for i=1:length(values)
+        result[index_l[i], j] += alpha * values[i] * B[index_r[i], j]
+    end
+end
+
+function gemm!{T}(alpha::T, M::SparseMatrix{T}, B::Matrix{T}, beta::T, result::Matrix{T})
+    for j=1:size(result,2), i=1:size(result,1)
+        result[i,j] *= beta
+    end
+    @inbounds for j=1:size(B,1)
+        _fMB(j, M.index_l, M.index_r, M.values, alpha, B, result)
+    end
+end
+
+function _fBM{T}(j::Int, index_l::Vector{Int}, index_r::Vector{Int}, values::Vector{T}, alpha::T, B::Matrix{T}, result::Matrix{T})
+    @inbounds for i=1:length(values)
+        result[j, index_r[i]] += alpha * values[i] * B[j, index_l[i]]
+    end
+end
+
+function gemm!{T}(alpha::T, B::Matrix{T}, M::SparseMatrix{T}, beta::T, result::Matrix{T})
+    for j=1:size(result,2), i=1:size(result,1)
+        result[i,j] *= beta
+    end
+    @inbounds for j=1:size(B,1)
+        _fBM(j, M.index_l, M.index_r, M.values, alpha, B, result)
+    end
+end
+
+
+end
