b809a83aa1a2dddfcd21d1739c27e9e4c050a99b
Sebastian Kraemer <basti.kr@gmail.com>
2014-09-12 04:10:55 +0200

    Update profiling script.

--- profiling/sparsematrix_compare_multiplications.jl
+++ profiling/sparsematrix_compare_multiplications.jl
@@ -1,6 +1,6 @@
-module test_sparse
+#module test_sparse
 
-export SparseMatrix
+#export SparseMatrix
 
 type SparseMatrix{T}
     index_l::Vector{Int}
@@ -33,38 +33,58 @@ end
 optimize_left!(S::SparseMatrix) = perm!(S, sortperm(S.index_l))
 optimize_right!(S::SparseMatrix) = perm!(S, sortperm(S.index_r))
 
+function inner_SB_js{T}(j::Int, index_l::Vector{Int}, index_r::Vector{Int}, values::Vector{T}, B::Matrix{T}, result::Matrix{T})
+    @inbounds for s=1:length(index_l)
+        result[index_l[s], j] += values[s] * B[index_r[s], j]
+    end
+    return nothing
+end
+
 function mul_SB_js{T}(S::SparseMatrix{T}, B::Matrix{T}, result::Matrix{T})
     @inbounds for j=1:size(B,1)
-        for s=1:length(S.index_l)
-            result[S.index_l[s], j] += S.values[s] * B[S.index_r[s], j]
-        end
+        inner_SB_js(j, S.index_l, S.index_r, S.values, B, result)
+    end
+    return nothing
+end
+
+function inner_SB_sj{T}(s::Int, index_l::Int, index_r::Int, value::T, B::Matrix{T}, result::Matrix{T})
+    @inbounds for j=1:size(B,1)
+        result[index_l, j] += value * B[index_r, j]
     end
     return nothing
 end
 
 function mul_SB_sj{T}(S::SparseMatrix{T}, B::Matrix{T}, result::Matrix{T})
     @inbounds for s=1:length(S.index_l)
-        for j=1:size(B,1)
-            result[S.index_l[s], j] += S.values[s] * B[S.index_r[s], j]
-        end
+        inner_SB_sj(s, S.index_l[s], S.index_r[s], S.values[s], B, result)
+    end
+    return nothing
+end
+
+function inner_BS_js{T}(i::Int, index_l::Vector{Int}, index_r::Vector{Int}, values::Vector{T}, B::Matrix{T}, result::Matrix{T})
+    @inbounds for s=1:length(index_l)
+        result[i, index_r[s]] += B[i, index_l[s]] * values[s]
     end
     return nothing
 end
 
 function mul_BS_js{T}(B::Matrix{T}, S::SparseMatrix{T}, result::Matrix{T})
     @inbounds for i=1:size(B,1)
-        for s=1:length(S.index_l)
-            result[i, S.index_r[s]] += B[i, S.index_l[s]] * S.values[s]
-        end
+        inner_BS_js(i, S.index_l, S.index_r, S.values, B, result)
+    end
+    return nothing
+end
+
+function inner_BS_sj{T}(s::Int, index_l::Int, index_r::Int, value::T, B::Matrix{T}, result::Matrix{T})
+    @inbounds for i=1:size(B,1)
+        result[i, index_r] += B[i, index_l] * value
     end
     return nothing
 end
 
 function mul_BS_sj{T}(B::Matrix{T}, S::SparseMatrix{T}, result::Matrix{T})
     @inbounds for s=1:length(S.index_l)
-        for i=1:size(B,1)
-            result[i, S.index_r[s]] += B[i, S.index_l[s]] * S.values[s]
-        end
+        inner_BS_sj(s, S.index_l[s], S.index_r[s], S.values[s], B, result)
     end
     return nothing
 end
@@ -85,45 +105,66 @@ function doprofile(N, fill=0.01, seed=0)
     mul_BS_js(B, S, result)
     mul_BS_sj(B, S, result)
 
+    T = Float64[]
+
     println("=========SxB=========")
     optimize_left!(S)
     fill!(result, 0)
-    @time mul_SB_js(S, B, result)
+    push!(T, @elapsed(mul_SB_js(S, B, result)))
     #println(norm(SB-result))
     fill!(result, 0)
-    @time mul_SB_sj(S, B, result)
+    push!(T, @elapsed(mul_SB_sj(S, B, result)))
     #println(norm(SB-result))
 
     optimize_right!(S)
     fill!(result, 0)
-    @time mul_SB_js(S, B, result)
+    push!(T, @elapsed(mul_SB_js(S, B, result)))
     #println(norm(SB-result))
     fill!(result, 0)
-    @time mul_SB_sj(S, B, result)
+    push!(T, @elapsed(mul_SB_sj(S, B, result)))
     #println(norm(SB-result))
 
 
     println("=========BxS=========")
     optimize_left!(S)
     fill!(result, 0)
-    @time mul_BS_js(B, S, result)
+    push!(T, @elapsed(mul_BS_js(B, S, result)))
     #println(norm(BS-result))
     fill!(result, 0)
-    @time mul_BS_sj(B, S, result)
+    push!(T, @elapsed(mul_BS_sj(B, S, result)))
     #println(norm(BS-result))
 
     optimize_right!(S)
     fill!(result, 0)
-    @time mul_BS_js(B, S, result)
+    push!(T, @elapsed(mul_BS_js(B, S, result)))
     #println(norm(BS-result))
     fill!(result, 0)
-    @time mul_BS_sj(B, S, result)
+    push!(T, @elapsed(mul_BS_sj(B, S, result)))
     #println(norm(BS-result))
+    return T
 end
 
+N = [10:10:1000]
+T = Array{Float64,2}[]
+for n=N
+    push!(T, doprofile(n, 0.1)')
+end
 
-doprofile(3000)
+T = vcat(T...)
 
+using Winston
+
+figure(height=2000,width=2000)
+hold(true)
+
+colors = ["y", "m", "c", "r", "g", "b", "w", "k"]
+
+for i=1:size(T,2)
+    println(T[:,i])
+    semilogy(N, T[:,i], colors[i])
+end
+savefig("t.png")
+#=
 error()
 
 
@@ -251,5 +292,6 @@ result = zeros(eltype(A), N, N)
 result = zeros(eltype(A), N, N)
 @time gemm!(1., M3, A, 0., result)
 # println(norm(result-r*A))
+=#
 
-end
+#end
