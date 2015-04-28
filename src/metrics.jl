b503cf2f7a9100131d4256aad4e7215b875f6554
Sebastian Kraemer <basti.kr@gmail.com>
2015-04-28 14:20:04 +0200

    Fix calculation of tracedistance.
    The Hermitian only allows diagonal elements that have an imaginary part
    that is strictly equal to zero. This means we now have to truncate
    imaginary numerical error.

--- src/metrics.jl
+++ src/metrics.jl
@@ -6,7 +6,12 @@ export tracedistance
 
 
 function tracedistance(rho::Operator, sigma::Operator)
-    s = eigvals(Hermitian((rho - sigma).data))
+    delta = (rho - sigma).data
+    @assert size(delta, 1) == size(delta, 2)
+    for i=1:size(delta,1)
+        delta[i,i] = real(delta[i,i])
+    end
+    s = eigvals(Hermitian(delta))
     return 0.5*sum(abs(s))
 end
 
