2d2085b511b07ea800173a9b7626a7c521c798f6
Sebastian Kraemer <basti.kr@gmail.com>
2015-09-25 17:02:17 +0200

    Transform states into Vectors when interfacing ODE.jl

--- src/timeevolution_simple.jl
+++ src/timeevolution_simple.jl
@@ -49,8 +49,14 @@ function master(T::Vector, rho0::Operator, H::AbstractOperator, J::Vector;
     if typeof(gamma)<:Real
         gamma = ones(typeof(gamma), length(J))*gamma
     end
-    f(t::Float64, rho::Operator) = dmaster(rho, H, gamma, J, Jdagger)
-    tout, rho_t = ode45(f, rho0, T; kwargs...)
+    nl = prod(rho0.basis_l.shape)
+    nr = prod(rho0.basis_r.shape)
+    N = nl*nr
+    as_operator(x::Vector{Complex128}) = Operator(rho0.basis_l, rho0.basis_r, reshape(x, nl, nr))
+    as_vector(rho::Operator) = reshape(rho.data, N)
+    f(t::Float64, x::Vector{Complex128}) = as_vector(dmaster(as_operator(x), H, gamma, J, Jdagger))
+    tout, x_t = ode45(f, as_vector(rho0), T; kwargs...)
+    rho_t = [as_operator(x) for x=x_t]
     return tout, rho_t
 end
 
@@ -74,10 +80,13 @@ end
 """
 Integrate schroedinger equation.
 """
-function schroedinger(T::Vector, psi0::StateVector, H::AbstractOperator; kwargs...)
-    f(t::Float64, psi::StateVector) = dschroedinger(psi, H)
-    tout, rho_t = ode45(f, psi0, T; kwargs...)
-    return tout, rho_t
+function schroedinger{T<:StateVector}(tspan::Vector, psi0::T, H::AbstractOperator; kwargs...)
+    as_statevector(x::Vector{Complex128}) = T(psi0.basis, x)
+    as_vector(psi::T) = psi.data
+    f(t::Float64, x::Vector{Complex128}) = as_vector(dschroedinger(as_statevector(x), H))
+    tout, x_t = ode45(f, as_vector(psi0), tspan; kwargs...)
+    psi_t = [as_statevector(x) for x=x_t]
+    return tout, psi_t
 end
 
 end  # module
