b994b8e8c5b30afd771fb80ad8de5a4df0c3c433
Sebastian Kraemer <basti.kr@gmail.com>
2015-10-06 10:10:05 +0200

    Clean up and write docstrings for spin.

--- src/spins.jl
+++ src/spins.jl
@@ -6,16 +6,36 @@ using ..bases, ..states, ..operators
 
 export SpinBasis, sigmax, sigmay, sigmaz, sigmap, sigmam
 
+
+"""
+Basis for spin-n particles.
+
+The basis can be created for arbitrary spinnumbers by using a rational number,
+e.g. SpinBasis(3//2). The pauli operators are defined for all possible
+spinnumbers.
+"""
 type SpinBasis <: Basis
     shape::Vector{Int}
     spinnumber::Rational{Int}
     function SpinBasis(spinnumber::Rational{Int})
         @assert den(spinnumber) == 2 || den(spinnumber) == 1
+        @assert den(spinnumber) > 0
         new([num(spinnumber*2 + 1)], spinnumber)
     end
 end
 
 ==(b1::SpinBasis, b2::SpinBasis) = b1.spinnumber==b2.spinnumber
+
+
+function sigmax(b::SpinBasis)
+    d = [complex(sqrt(real((b.spinnumber + 1)*2*a - a*(a+1)))) for a=1:num(2*b.spinnumber)]
+    Operator(b, diagm(d,1) + diagm(d,-1))
+end
+
+function sigmay(b::SpinBasis)
+    d = [1im*complex(sqrt(real((b.spinnumber + 1)*2*a - a*(a+1)))) for a=1:num(2*b.spinnumber)]
+    Operator(b, diagm(d,-1) - diagm(d,1))
+end
 
 function sigmaz(b::SpinBasis)
     Operator(b, diagm([complex(2*m) for m=b.spinnumber:-1:-b.spinnumber]))
@@ -33,17 +53,5 @@ function sigmam(b::SpinBasis)
     Operator(b, diagm(d, -1))
 end
 
-function sigmax(b::SpinBasis)
-    d = [complex(sqrt(real((b.spinnumber + 1)*2*a - a*(a+1)))) for a=1:num(2*b.spinnumber)]
-    Operator(b, diagm(d,1) + diagm(d,-1))
-end
-
-function sigmay(b::SpinBasis)
-    d = [1im*complex(sqrt(real((b.spinnumber + 1)*2*a - a*(a+1)))) for a=1:num(2*b.spinnumber)]
-    Operator(b, diagm(d,-1) - diagm(d,1))
-end
-
-# sigmax(b::SpinBasis) = (sigmap(b) + sigmam(b))
-# sigmay(b::SpinBasis) = (sigmap(b) - sigmam(b))/1im
 
 end #module
