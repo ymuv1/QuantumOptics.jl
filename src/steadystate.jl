846d0900b13d1a539e4cd9d54ffe1ada91b6238c
Sebastian Kraemer <basti.kr@gmail.com>
2015-10-29 14:41:03 +0100

    Use master instead of master_nh in steadystate calculation.

--- src/steadystate.jl
+++ src/steadystate.jl
@@ -11,7 +11,7 @@ type ConvergenceReached <: Exception end
 function master(H::AbstractOperator, J::Vector;
                 rho0::Operator=tensor(basis_ket(H.basis_l, 1), basis_bra(H.basis_r, 1)),
                 eps::Float64=1e-3, hmin=1e-7,
-                Gamma::Union{Real, Vector, Matrix}=ones(Float64, length(J)),
+                Gamma::Union{Vector{Float64}, Matrix{Float64}}=ones(Float64, length(J)),
                 Jdagger::Vector=map(dagger, J),
                 fout::Union{Function,Void}=nothing,
                 tmp::Operator=deepcopy(rho0),
@@ -31,7 +31,7 @@ function master(H::AbstractOperator, J::Vector;
         end
     end
     try
-        timeevolution.master_h([0., Inf], rho0, H, J; Gamma=Gamma, Jdagger=Jdagger,
+        timeevolution.master([0., Inf], rho0, H, J; Gamma=Gamma, Jdagger=Jdagger,
                             hmin=hmin, hmax=Inf,
                             tmp=tmp,
                             display_initialvalue=false,
