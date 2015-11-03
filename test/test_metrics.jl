e91719cd87035bf1b7062c0c8291bb3838f071c6
Sebastian Kraemer <basti.kr@gmail.com>
2015-11-03 09:17:20 +0100

    Add test for metrics.

---
+++ test/test_metrics.jl
@@ -0,0 +1,14 @@
+using Base.Test
+using quantumoptics
+
+b = SpinBasis(1//2)
+
+psi1 = spinup(b)
+psi2 = spindown(b)
+
+rho = tensor(psi1, dagger(psi1))
+sigma = tensor(psi2, dagger(psi2))
+
+@test tracedistance(rho, sigma) == 1.
+@test tracedistance(rho, rho) == 0.
+@test tracedistance(sigma, sigma) == 0.
