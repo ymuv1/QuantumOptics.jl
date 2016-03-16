4afd1818dd97f066e8df5700e578917b13f7336b
Sebastian Kraemer <basti.kr@gmail.com>
2016-03-16 10:52:48 +0100

    Implement trace distance for sparse matrices.

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,6 +1,6 @@
 module metrics
 
-using ..operators
+using ..operators, ..operators_sparse
 
 export tracedistance
 
@@ -12,6 +12,17 @@ function tracedistance(rho::DenseOperator, sigma::DenseOperator)
         delta[i,i] = real(delta[i,i])
     end
     s = eigvals(Hermitian(delta))
+    return 0.5*sum(abs(s))
+end
+
+function tracedistance(rho::SparseOperator, sigma::SparseOperator)
+    N = size(rho.data, 1)
+    if N < 10
+        return tracedistance(full(rho), full(sigma))
+    end
+    delta = (rho - sigma).data
+    @assert size(delta, 1) == size(delta, 2)
+    s, v, nconv, niter, nmult, resid = Base.eigs(delta; nev=N-2, ncv=N, sigma=1e-30)
     return 0.5*sum(abs(s))
 end
 
