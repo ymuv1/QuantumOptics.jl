acd5e242e5f54c3e3eebb4be6f2d7d2dac7096d4
David Plankensteiner <david.plankensteiner@uibk.ac.at>
2016-12-22 14:16:38 +0100

    Deleted time output when testing.

--- test/test_nparticles.jl
+++ test/test_nparticles.jl
@@ -11,12 +11,8 @@ op = DenseOperator(GenericBasis([modenumber]), m)
 op2 = SparseOperator(GenericBasis([modenumber]), sparse(m))
 op_ = nparticleoperator_1(b, op)
 op2_ = nparticleoperator_1(b, op2)
-tic()
 op_ = nparticleoperator_1(b, op)
-toc()
-tic()
 op2_ = nparticleoperator_1(b, op2)
-toc()
 
 @test tracedistance(op_, full(op2_)) < 1e-12
 
@@ -27,17 +23,7 @@ op = DenseOperator(GenericBasis([modenumber^2]), m)
 op2 = SparseOperator(GenericBasis([modenumber^2]), sparse(m))
 op_ = nparticleoperator_2(b, op)
 op2_ = nparticleoperator_2(b, op2)
-tic()
 op_ = nparticleoperator_2(b, op)
-toc()
-tic()
 op2_ = nparticleoperator_2(b, op2)
-toc()
 
 @test tracedistance(op_, full(op2_)) < 1e-12
-
-# print(op_)
-# print(op2_)
-
-# println(length(b.occupations))
-# println(length(b))
