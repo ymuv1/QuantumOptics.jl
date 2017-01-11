a51ca848de16991a833718c65356d5bf85a3a79e
Sebastian Kraemer <basti.kr@gmail.com>
2017-01-11 12:24:56 +0100

    Improve n-level implementation.
    * Implement nlevelstate function.
    * Better index boundary check.
    * Docstrings.
    * Tests.

--- src/nlevel.jl
+++ src/nlevel.jl
@@ -4,9 +4,17 @@ import Base.==
 
 using ..bases, ..states, ..operators, ..operators_sparse
 
-export NLevelBasis, transition
+export NLevelBasis, transition, nlevelstate
 
+"""
+Basis for a system consisting of N states.
 
+Arguments
+---------
+
+N
+    Number of states.
+"""
 type NLevelBasis <: Basis
     shape::Vector{Int}
     N::Int
@@ -20,11 +28,41 @@ end
 
 ==(b1::NLevelBasis, b2::NLevelBasis) = b1.N == b2.N
 
+
+"""
+Transition operator :math:`|n><m|`.
+
+Arguments
+---------
+
+b
+    NLevelBasis
+to
+    Index of ket state in :math:`|to><from|`
+from
+    Index of bra state in :math:`|to><from|`
+"""
 function transition(b::NLevelBasis, to::Int, from::Int)
+    if to < 1 || b.N < to
+        throw(BoundsError("'to' index has to be between 1 and b.N"))
+    end
+    if from < 1 || b.N < from
+        throw(BoundsError("'from' index has to be between 1 and b.N"))
+    end
     op = SparseOperator(b)
     op.data[to, from] = 1.
     op
 end
 
+
+"""
+State where the system is completely in the n-th level.
+"""
+function nlevelstate(b::NLevelBasis, n::Int)
+    if n < 1 || b.N < n
+        throw(BoundsError("n has to be between 1 and b.N"))
+    end
+    basis_ket(b, n)
+end
 
 end # module
--- test/test_nlevel.jl
+++ test/test_nlevel.jl
@@ -1,10 +1,24 @@
 using Base.Test
 using QuantumOptics
 
-energies = [0., 1.1, 2.6]
+
 N = 3
 b = NLevelBasis(N)
 
+# Test basis equality
 @test b == NLevelBasis(N)
 @test b != NLevelBasis(N+1)
+
+# Test transition operator
+@test_throws BoundsError transition(b, 0, 1)
+@test_throws BoundsError transition(b, N+1, 1)
+@test_throws BoundsError transition(b, 1, 0)
+@test_throws BoundsError transition(b, 1, N+1)
 @test full(transition(b, 2, 1)) == basis_ket(b, 2) ⊗ basis_bra(b, 1)
+
+# Test nlevel states
+@test_throws BoundsError nlevelstate(b, 0)
+@test_throws BoundsError nlevelstate(b, N+1)
+@test norm(nlevelstate(b, 1)) == 1.
+@test norm(dagger(nlevelstate(b, 1))*nlevelstate(b, 2)) == 0.
+@test norm(dagger(nlevelstate(b, 1))*transition(b, 1, 2)*nlevelstate(b, 2)) == 1.
