8d0c0f99d32e936276dfe69d721351ea5c1aac67
David Plankensteiner <david.plankensteiner@uibk.ac.at>
2017-02-07 16:31:31 +0100

    Add line to convert notebook examples to jl files

--- docs/convertnotebooks.sh
+++ docs/convertnotebooks.sh
@@ -5,4 +5,5 @@ BUILDPATH=source/examples/
 for f in $SOURCEPATH*.ipynb
 do
     jupyter nbconvert --to=rst --FilesWriter.build_directory=$BUILDPATH "$f"
+    jupyter nbconvert --to script --FilesWriter.build_directory=$SOURCEPATH "$f"
 done
