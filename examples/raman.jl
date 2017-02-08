da7c3ebca3d95153055cf4279e81f72c33ae66a6
David Plankensteiner <david.plankensteiner@uibk.ac.at>
2017-02-08 10:43:52 +0100

    Add the Raman example in notebook form

--- examples/raman.jl
+++ examples/raman.jl
@@ -1,13 +1,6 @@
+
 using QuantumOptics
 using PyPlot
-
-"""
-Example that illustrates the concept of a Raman transition. A Λ-scheme
-three-level atom is prepared in one of two ground states (state 1 in the
-following code) and is driven by a laser that is far-detuned from the
-transition to the excited state. It spontaneously decays into the other ground
-state.
-"""
 
 # Parameters
 γ₃ = 1.
@@ -16,29 +9,29 @@ state.
 Δ₃ = 0.0
 tmax = 800/γ₃
 dt = 0.1
-tlist = [0:dt:tmax;]
+tlist = [0:dt:tmax;];
 
 # Basis and operators
 b = NLevelBasis(3)
 σ₁ = transition(b, 1, 2)
 σ₃ = transition(b, 3, 2)
 proj₂ = transition(b, 2, 2)
-proj₃ = σ₃*dagger(σ₃)
+proj₃ = σ₃*dagger(σ₃);
 
 # Hamiltonian and jump operators
 H = Δ₂*proj₂ + Δ₃*proj₃ + Ω*(σ₁ + dagger(σ₁))
-J = [sqrt(γ₃)*σ₃]
+J = [sqrt(γ₃)*σ₃];
 
 # Initial state
 ψ₀ = nlevelstate(b, 1)
 
 # Time evolution
-tout, ρₜ = timeevolution.master(tlist, ψ₀, H, J)
+tout, ρₜ = timeevolution.master(tlist, ψ₀, H, J);
 
 # Expectation values
 p1 = real(expect(σ₁*dagger(σ₁), ρₜ))
 p2 = real(expect(proj₂, ρₜ))
-p3 = real(expect(proj₃, ρₜ))
+p3 = real(expect(proj₃, ρₜ));
 
 # Plots
 clf()
@@ -48,3 +41,5 @@ plot(tout, p3, "r", label="Other ground state")
 axis([0, tmax, 0, 1])
 legend()
 show()
+
+
