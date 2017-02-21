1b8352cf53a9be8143a11e1713d7cb22c99700eb
Sebastian Kraemer <basti.kr@gmail.com>
2017-02-21 13:36:31 +0100

    More efficient checking of arguments for ptrace and embed.

--- src/operators.jl
+++ src/operators.jl
@@ -125,9 +125,8 @@ function embed{T<:Operator}(basis_l::CompositeBasis, basis_r::CompositeBasis,
                             indices::Vector{Int}, operators::Vector{T})
     N = length(basis_l.bases)
     @assert length(basis_r.bases) == N
-    @assert length(indices) == length(Set(indices)) == length(operators)
-    @assert maximum(indices) <= N
-    @assert minimum(indices) > 0
+    @assert length(indices) == length(operators)
+    check_indices(N, indices)
     tensor([i ∈ indices ? operators[findfirst(indices, i)] : identityoperator(T, basis_l.bases[i], basis_r.bases[i]) for i=1:N]...)
 end
 embed(basis_l::CompositeBasis, basis_r::CompositeBasis, index::Int, op::Operator) = embed(basis_l, basis_r, Int[index], [op])
@@ -183,14 +182,28 @@ embed{T<:Operator}(basis::CompositeBasis, operators::Dict{Vector{Int}, T}; kwarg
 gemv!() = error("Not Implemented.")
 gemm!() = error("Not Implemented.")
 
+
 # Helper functions to check validity of arguments
+function check_indices(imax::Int, indices::Vector{Int})
+    N = 0
+    for i=1:imax
+        n = count(x->x==i, indices)
+        N += n
+        @assert n == 1 || n == 0
+    end
+    @assert length(indices) == N
+end
+
 function check_ptrace_arguments(a::Operator, indices::Vector{Int})
     @assert length(a.basis_l.shape) == length(a.basis_r.shape)
-    S = Set(indices)
-    @assert length(S) == length(indices)
-    @assert issubset(S, Set(1:length(a.basis_l.shape)))
+    check_indices(length(a.basis_l.shape), indices)
 end
-check_samebases(a::Operator, b::Operator) = ((a.basis_l!=b.basis_l) || (a.basis_r!=b.basis_r) ? throw(IncompatibleBases()) : nothing)
+
+function check_samebases(a::Operator, b::Operator)
+    if (a.basis_l!=b.basis_l) || (a.basis_r!=b.basis_r)
+        throw(IncompatibleBases())
+    end
+end
 
 
 end # module
