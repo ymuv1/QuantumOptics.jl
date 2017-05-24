9d419d0fc2a224a0e51c496f6b775c23357deef2
Sebastian Kraemer <basti.kr@gmail.com>
2017-05-24 09:49:46 +0200

    Improve handling of rates in master time evolution.

--- src/master.jl
+++ src/master.jl
@@ -8,6 +8,8 @@ using ...states
 using ...operators
 using ...operators_dense
 
+typealias DecayRates Union{Vector{Float64}, Matrix{Float64}, Void}
+
 
 function recast!(x::Vector{Complex128}, rho::DenseOperator)
     rho.data = reshape(x, size(rho.data))
@@ -17,11 +19,26 @@ function recast!(rho::DenseOperator, x::Vector{Complex128})
     nothing
 end
 
-"""
-Evaluate master equation for diagonal jump operators.
-"""
+
 function dmaster_h(rho::DenseOperator, H::Operator,
-                    Gamma::Vector{Complex128}, J::Vector, Jdagger::Vector,
+                    Gamma::Void, J::Vector, Jdagger::Vector,
+                    drho::DenseOperator, tmp::DenseOperator)
+    operators.gemm!(complex(0,-1.), H, rho, complex(0.), drho)
+    operators.gemm!(complex(0,1.), rho, H, complex(1.), drho)
+    for i=1:length(J)
+        operators.gemm!(1., J[i], rho, complex(0.), tmp)
+        operators.gemm!(complex(1.), tmp, Jdagger[i], complex(1.), drho)
+
+        operators.gemm!(complex(-0.5), Jdagger[i], tmp, complex(1.), drho)
+
+        operators.gemm!(1., rho, Jdagger[i], complex(0.), tmp)
+        operators.gemm!(complex(-0.5), tmp, J[i], complex(1.), drho)
+    end
+    return drho
+end
+
+function dmaster_h(rho::DenseOperator, H::Operator,
+                    Gamma::Vector{Float64}, J::Vector, Jdagger::Vector,
                     drho::DenseOperator, tmp::DenseOperator)
     operators.gemm!(complex(0,-1.), H, rho, complex(0.), drho)
     operators.gemm!(complex(0,1.), rho, H, complex(1.), drho)
@@ -37,11 +54,8 @@ function dmaster_h(rho::DenseOperator, H::Operator,
     return drho
 end
 
-"""
-Evaluate master equation for non-diagonal jump operators.
-"""
 function dmaster_h(rho::DenseOperator, H::Operator,
-                    Gamma::Matrix{Complex128}, J::Vector, Jdagger::Vector,
+                    Gamma::Matrix{Float64}, J::Vector, Jdagger::Vector,
                     drho::DenseOperator, tmp::DenseOperator)
     operators.gemm!(complex(0,-1.), H, rho, complex(0.), drho)
     operators.gemm!(complex(0,1.), rho, H, complex(1.), drho)
@@ -57,26 +71,28 @@ function dmaster_h(rho::DenseOperator, H::Operator,
     return drho
 end
 
-"""
-Evaluate master equation for dynamic operators.
-"""
 function dmaster_h_dynamic(t::Float64, rho::DenseOperator, f::Function,
-                    Gamma::Union{Vector{Float64}, Matrix{Float64}, Void},
+                    Gamma::DecayRates,
                     drho::DenseOperator, tmp::DenseOperator)
       H, J, Jdagger = f(t, rho)
-      if isa(Gamma, Void)
-        Gamma = ones(Float64, length(J))
-      end
       _check_input(rho, H, J, Jdagger, Gamma)
-      Gamma_ = complex(Gamma)
-      dmaster_h(rho, H, Gamma_, J, Jdagger, drho, tmp)
+      dmaster_h(rho, H, Gamma, J, Jdagger, drho, tmp)
 end
 
-"""
-Evaluate master equation for non-hermitian Hamiltonian and diagonal jump operators.
-"""
 function dmaster_nh(rho::DenseOperator, Hnh::Operator, Hnh_dagger::Operator,
-                    Gamma::Vector{Complex128}, J::Vector, Jdagger::Vector,
+                    Gamma::Void, J::Vector, Jdagger::Vector,
+                    drho::DenseOperator, tmp::DenseOperator)
+    operators.gemm!(complex(0,-1.), Hnh, rho, complex(0.), drho)
+    operators.gemm!(complex(0,1.), rho, Hnh_dagger, complex(1.), drho)
+    for i=1:length(J)
+        operators.gemm!(1., J[i], rho, complex(0.), tmp)
+        operators.gemm!(complex(1.), tmp, Jdagger[i], complex(1.), drho)
+    end
+    return drho
+end
+
+function dmaster_nh(rho::DenseOperator, Hnh::Operator, Hnh_dagger::Operator,
+                    Gamma::Vector{Float64}, J::Vector, Jdagger::Vector,
                     drho::DenseOperator, tmp::DenseOperator)
     operators.gemm!(complex(0,-1.), Hnh, rho, complex(0.), drho)
     operators.gemm!(complex(0,1.), rho, Hnh_dagger, complex(1.), drho)
@@ -91,7 +107,7 @@ end
 Evaluate master equation for non-hermitian Hamiltonian and non-diagonal jump operators.
 """
 function dmaster_nh(rho::DenseOperator, Hnh::Operator, Hnh_dagger::Operator,
-                    Gamma::Matrix{Complex128}, J::Vector, Jdagger::Vector,
+                    Gamma::Matrix{Float64}, J::Vector, Jdagger::Vector,
                     drho::DenseOperator, tmp::DenseOperator)
     operators.gemm!(complex(0,-1.), Hnh, rho, complex(0.), drho)
     operators.gemm!(complex(0,1.), rho, Hnh_dagger, complex(1.), drho)
@@ -106,19 +122,15 @@ end
 Evaluate master equation for dynamic operators.
 """
 function dmaster_nh_dynamic(t::Float64, rho::DenseOperator, f::Function,
-                    Gamma::Union{Vector{Float64}, Matrix{Float64}, Void},
+                    Gamma::DecayRates,
                     drho::DenseOperator, tmp::DenseOperator)
       Hnh, Hnh_dagger, J, Jdagger = f(t, rho)
-      if isa(Gamma, Void)
-        Gamma = ones(Float64, length(J))
-      end
       _check_input(rho, Hnh, J, Jdagger, Gamma)
-      Gamma_ = complex(Gamma)
-      dmaster_nh(rho, Hnh, Hnh_dagger, Gamma_, J, Jdagger, drho, tmp)
+      dmaster_nh(rho, Hnh, Hnh_dagger, Gamma, J, Jdagger, drho, tmp)
 end
 
 
-function _check_input(rho0::DenseOperator, H::Operator, J::Vector, Jdagger::Vector, Gamma::Union{Vector{Float64}, Matrix{Float64}})
+function _check_input(rho0::DenseOperator, H::Operator, J::Vector, Jdagger::Vector, Gamma::DecayRates)
     check_samebases(rho0, H)
     for j=J
         @assert typeof(j) <: Operator
@@ -144,14 +156,13 @@ Integrate the master equation with dmaster_h as derivative function.
 Further information can be found at [`master`](@ref).
 """
 function master_h(tspan, rho0::DenseOperator, H::Operator, J::Vector;
-                Gamma::Union{Vector{Float64}, Matrix{Float64}}=ones(Float64, length(J)),
+                Gamma::DecayRates=nothing,
                 Jdagger::Vector=dagger.(J),
                 fout::Union{Function,Void}=nothing,
                 tmp::DenseOperator=copy(rho0),
                 kwargs...)
     tspan_ = convert(Vector{Float64}, tspan)
     _check_input(rho0, H, J, Jdagger, Gamma)
-    Gamma = complex(Gamma)
     dmaster_(t, rho::DenseOperator, drho::DenseOperator) = dmaster_h(rho, H, Gamma, J, Jdagger, drho, tmp)
     x0 = reshape(rho0.data, length(rho0))
     state = DenseOperator(rho0.basis_l, rho0.basis_r, rho0.data)
@@ -176,7 +187,7 @@ H_{nh} = H - \\frac{i}{2} \\sum_k J^†_k J_k
 Further information can be found at [`master`](@ref).
 """
 function master_nh(tspan, rho0::DenseOperator, Hnh::Operator, J::Vector;
-                Gamma::Union{Vector{Float64}, Matrix{Float64}}=ones(Float64, length(J)),
+                Gamma::DecayRates=nothing,
                 Hnhdagger::Operator=dagger(Hnh),
                 Jdagger::Vector=dagger.(J),
                 fout::Union{Function,Void}=nothing,
@@ -184,7 +195,6 @@ function master_nh(tspan, rho0::DenseOperator, Hnh::Operator, J::Vector;
                 kwargs...)
     tspan_ = convert(Vector{Float64}, tspan)
     _check_input(rho0, Hnh, J, Jdagger, Gamma)
-    Gamma = complex(Gamma)
     dmaster_(t, rho::DenseOperator, drho::DenseOperator) = dmaster_nh(rho, Hnh, Hnhdagger, Gamma, J, Jdagger, drho, tmp)
     x0 = reshape(rho0.data, length(rho0))
     state = DenseOperator(rho0.basis_l, rho0.basis_r, rho0.data)
@@ -216,8 +226,9 @@ non-hermitian Hamiltonian and then calls master_nh which is slightly faster.
 * `H`: Arbitrary operator specifying the Hamiltonian.
 * `J`: Vector containing all jump operators which can be of any arbitrary
         operator type.
-* `Gamma=ones(N)`: Vector or matrix specifying the coefficients (decay rates)
-        for the jump operators.
+* `Gamma=nothing`: Vector or matrix specifying the coefficients (decay rates)
+        for the jump operators. If nothing is specified all rates are assumed
+        to be 1.
 * `Jdagger=dagger.(J)`: Vector containing the hermitian conjugates of the jump
         operators. If they are not given they are calculated automatically.
 * `fout=nothing`: If given, this function `fout(t, rho)` is called every time
@@ -227,14 +238,13 @@ non-hermitian Hamiltonian and then calls master_nh which is slightly faster.
 * `kwargs...`: Further arguments are passed on to the ode solver.
 """
 function master(tspan, rho0::DenseOperator, H::Operator, J::Vector;
-                Gamma::Union{Vector{Float64}, Matrix{Float64}}=ones(Float64, length(J)),
+                Gamma::DecayRates=nothing,
                 Jdagger::Vector=dagger.(J),
                 fout::Union{Function,Void}=nothing,
                 tmp::DenseOperator=copy(rho0),
                 kwargs...)
     tspan_ = convert(Vector{Float64}, tspan)
     _check_input(rho0, H, J, Jdagger, Gamma)
-    Gamma = complex(Gamma)
     dmaster_(t, rho::DenseOperator, drho::DenseOperator) = dmaster_h(rho, H, Gamma, J, Jdagger, drho, tmp)
     x0 = reshape(rho0.data, length(rho0))
     state = DenseOperator(rho0.basis_l, rho0.basis_r, rho0.data)
@@ -243,7 +253,7 @@ function master(tspan, rho0::DenseOperator, H::Operator, J::Vector;
 end
 
 function master(tspan, rho0::DenseOperator, H::DenseOperator, J::Vector{DenseOperator};
-                Gamma::Union{Vector{Float64}, Matrix{Float64}}=ones(Float64, length(J)),
+                Gamma::DecayRates=nothing,
                 Jdagger::Vector{DenseOperator}=dagger.(J),
                 fout::Union{Function,Void}=nothing,
                 tmp::DenseOperator=copy(rho0),
@@ -259,9 +269,12 @@ function master(tspan, rho0::DenseOperator, H::DenseOperator, J::Vector{DenseOpe
         for i=1:length(J)
             Hnh -= 0.5im*Gamma[i]*Jdagger[i]*J[i]
         end
+    else
+        for i=1:length(J)
+            Hnh -= 0.5im*Jdagger[i]*J[i]
+        end
     end
     Hnhdagger = dagger(Hnh)
-    Gamma = complex(Gamma)
     dmaster_(t, rho::DenseOperator, drho::DenseOperator) = dmaster_nh(rho, Hnh, Hnhdagger, Gamma, J, Jdagger, drho, tmp)
     x0 = reshape(rho0.data, length(rho0))
     state = DenseOperator(rho0.basis_l, rho0.basis_r, rho0.data)
@@ -286,7 +299,7 @@ H_{nh} = H - \\frac{i}{2} \\sum_k J^†_k J_k
 For further information look at [`master_dynamic`](@ref).
 """
 function master_nh_dynamic(tspan, rho0::DenseOperator, f::Function;
-                Gamma::Union{Vector{Float64}, Matrix{Float64}, Void}=nothing,
+                Gamma::DecayRates=nothing,
                 fout::Union{Function,Void}=nothing,
                 tmp::DenseOperator=copy(rho0),
                 kwargs...)
@@ -318,8 +331,9 @@ operators:
         automatically converted into a density operator.
 * `f`: Function `f(t, rho) -> (H, J, Jdagger)` returning the time and/or state dependent
         Hamiltonian and Jump operators.
-* `Gamma=ones(N)`: Vector or matrix specifying the coefficients (decay rates)
-        for the jump operators.
+* `Gamma=nothing`: Vector or matrix specifying the coefficients (decay rates)
+        for the jump operators. If nothing is specified all rates are assumed
+        to be 1.
 * `fout=nothing`: If given, this function `fout(t, rho)` is called every time
         an output should be displayed. ATTENTION: The given state rho is not
         permanent! It is still in use by the ode solver and therefore must not
@@ -327,7 +341,7 @@ operators:
 * `kwargs...`: Further arguments are passed on to the ode solver.
 """
 function master_dynamic(tspan, rho0::DenseOperator, f::Function;
-                Gamma::Union{Vector{Float64}, Matrix{Float64}, Void}=nothing,
+                Gamma::DecayRates=nothing,
                 fout::Union{Function,Void}=nothing,
                 tmp::DenseOperator=copy(rho0),
                 kwargs...)
