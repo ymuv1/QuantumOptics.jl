c5efb1435ac6d7b53eaea925bd5ccffb5b9e4fbf
david-pl <david-pl@users.noreply.github.com>
2018-01-03 11:29:11 +0100

    Implement ptranspose and related entanglement measures (#180)
    * Implement ptranspose, PPT, and entanglement measures
    
    * Small changes, add export and tests

--- src/metrics.jl
+++ src/metrics.jl
@@ -2,7 +2,8 @@ module metrics
 
 export tracenorm, tracenorm_h, tracenorm_nh,
         tracedistance, tracedistance_h, tracedistance_nh,
-        entropy_vn, fidelity
+        entropy_vn, fidelity, ptranspose, PPT, negativity,
+        logarithmic_negativity
 
 using ..bases, ..operators, ..operators_dense
 
@@ -154,7 +155,7 @@ entropy_vn(rho::DenseOperator) = sum([d == 0 ? 0 : -d*log(d) for d=eigvals(rho.d
 
 Fidelity of two density operators.
 
-The fidelity of two density operators ``\ρ`` and ``σ`` is defined by
+The fidelity of two density operators ``ρ`` and ``σ`` is defined by
 
 ```math
 F(ρ, σ) = Tr\\left(\\sqrt{\\sqrt{ρ}σ\\sqrt{ρ}}\\right),
@@ -163,5 +164,70 @@ F(ρ, σ) = Tr\\left(\\sqrt{\\sqrt{ρ}σ\\sqrt{ρ}}\\right),
 where ``\\sqrt{ρ}=\\sum_n\\sqrt{λ_n}|ψ⟩⟨ψ|``.
 """
 fidelity(rho::DenseOperator, sigma::DenseOperator) = trace(sqrtm(sqrtm(rho.data)*sigma.data*sqrtm(rho.data)))
+
+
+"""
+    ptranspose(rho, index)
+
+Partial transpose of rho with respect to subspace specified by index.
+"""
+function ptranspose(rho::DenseOperator, index::Int=1)
+    @assert rho.basis_l == rho.basis_r
+    @assert typeof(rho.basis_l) == CompositeBasis
+
+    # Define permutation
+    N = length(rho.basis_l.bases)
+    perm = [1:N;]
+    perm[index] = N
+    perm[N] = index
+
+    # Permute indexed subsystem to last position
+    rho_perm = permutesystems(rho, perm)
+
+    # Transpose corresponding blocks
+    m = Int(prod(rho_perm.basis_l.shape[1:N-1]))
+    n = rho_perm.basis_l.shape[N]
+    for i=1:n, j=1:n
+        rho_perm.data[m*(i-1)+1:m*i, m*(j-1)+1:m*j] = transpose(rho_perm.data[m*(i-1)+1:m*i, m*(j-1)+1:m*j])
+    end
+
+    return permutesystems(rho_perm, perm)
+end
+
+
+"""
+    PPT(rho, index)
+
+Peres-Horodecki criterion of partial transpose.
+"""
+PPT(rho::DenseOperator, index::Int) = all(real.(eigvals(ptranspose(rho, index).data)) .>= 0.0)
+
+
+"""
+    negativity(rho, index)
+
+Negativity of rho with respect to subsystem index.
+
+The negativity of a density matrix ρ is defined as
+
+```math
+N(ρ) = \|ρᵀ\|,
+```
+where `ρᵀ` is the partial transpose.
+"""
+negativity(rho::DenseOperator, index::Int) = 0.5*(tracenorm(ptranspose(rho, index)) - 1.0)
+
+
+"""
+    logarithmic_negativity(rho, index)
+
+The logarithmic negativity of a density matrix ρ is defined as
+
+```math
+N(ρ) = \log₂\|ρᵀ\|,
+```
+where `ρᵀ` is the partial transpose.
+"""
+logarithmic_negativity(rho::DenseOperator, index::Int) = log(2, tracenorm(ptranspose(rho, index)))
 
 end # module
--- test/test_metrics.jl
+++ test/test_metrics.jl
@@ -62,4 +62,29 @@ rho = tensor(psi1, dagger(psi1))
 @test fidelity(rho, rho) ≈ 1
 @test 1e-20 > abs2(fidelity(rho, sigma))
 
-end
+# ptranspose
+e = spinup(b1)
+g = spindown(b1)
+psi3 = (e ⊗ g - g ⊗ e)/sqrt(2)
+
+rho = dm(psi3)
+rho_pT1 = ptranspose(rho, 1)
+rho_pT1_an = 0.5*(dm(e ⊗ g) + dm(g ⊗ e) - (e ⊗ e) ⊗ dagger(g ⊗ g) - (g ⊗ g) ⊗ dagger(e ⊗ e))
+rho_pT2 = ptranspose(rho, 2)
+@test rho_pT1.data ≈ rho_pT1_an.data
+@test rho_pT2.data ≈ rho_pT1_an.data
+
+@test_throws AssertionError ptranspose(e ⊗ dagger(psi1))
+@test_throws AssertionError ptranspose(dm(e))
+
+rho = rho ⊗ dm(g)
+@test PPT(rho, 1) == PPT(rho, 2) == false
+@test PPT(rho, 3)
+
+@test negativity(rho, 1) == negativity(rho, 2) ≈ 0.5
+@test isapprox(negativity(rho, 3), 0.0, atol=1e-15)
+
+@test logarithmic_negativity(rho, 1) == logarithmic_negativity(rho, 2) ≈ 1.0
+@test isapprox(logarithmic_negativity(rho, 3), 0.0, atol=1e-15)
+
+end # testset
